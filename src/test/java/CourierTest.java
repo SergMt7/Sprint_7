e28d76d4b54b2437e2ffb8a67e5103369d043485
Sergey Minaylov <sergeymoto7@yandex.ru>
2023-01-22 17:04:35 +0300

    add allure report #2

--- src/test/java/CourierTest.java
+++ src/test/java/CourierTest.java
@@ -5,6 +5,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
 import model.CourierGenerator;
 import model.Courier;
 import model.CourierClient;
@@ -23,16 +25,16 @@ public class CourierTest {
 
     @Test
     @DisplayName("Создание нового курьера")
-    public void CreateNewCourier() {
+    public void сreateNewCourier() {
         ValidatableResponse response = courierClient.create(courier);
-        response.statusCode(201);
         ValidatableResponse loginResponse = courierClient.login(Login.from(courier));
         id = loginResponse.extract().path("id").toString();
+        response.statusCode(201);
     }
 
     @Test
     @DisplayName("Попытка создания курьера без логина")
-    public void CreateNewCourierWithoutLogin() {
+    public void сreateNewCourierWithoutLogin() {
         courier.setLogin("");
         ValidatableResponse response = courierClient.create(courier);
         response.assertThat().body("message", equalTo("Недостаточно данных для создания учетной записи"))
@@ -41,7 +43,7 @@ public class CourierTest {
 
     @Test
     @DisplayName("Попытка создания курьера без пароля")
-    public void CreateNewCourierWithoutPassword() {
+    public void сreateNewCourierWithoutPassword() {
         courier.setPassword("");
         ValidatableResponse response = courierClient.create(courier);
         response.assertThat().body("message", equalTo("Недостаточно данных для создания учетной записи"))
@@ -50,7 +52,7 @@ public class CourierTest {
 
     @Test
     @DisplayName("Попытка дублирования курьера с одинаковым логином")
-    public void CreatureDoubleNewCourier() {
+    public void сreatureDoubleNewCourier() {
         courierClient.create(courier);
         ValidatableResponse response = courierClient.create(courier);
         response.assertThat().body("message", equalTo("Этот логин уже используется. Попробуйте другой."))
